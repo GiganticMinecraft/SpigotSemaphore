52024a0bfef75345297290cb0aa5e5c5b672d916
amata1219 <suzukiamata@gmail.com>
2020-10-01 00:19:04 +0900

    :recycle: futureの待機処理をrunTask()中で実行するように変更

--- src/azisaba/semaphore/spigot/listener/PlayerQuitListener.scala
+++ src/azisaba/semaphore/spigot/listener/PlayerQuitListener.scala
@@ -6,11 +6,11 @@ import amata1219.redis.plugin.messages.spigot.RedisPluginMessagesAPI
 import azisaba.semaphore.spigot.SpigotSemaphore
 import azisaba.semaphore.spigot.future.FuturesCompletionWaiting
 import azisaba.semaphore.spigot.hook.{CoordinationHookRegistry, QuitEventDataSaveHook}
+import org.bukkit.Bukkit
 import org.bukkit.event.player.PlayerQuitEvent
 import org.bukkit.event.{EventHandler, EventPriority, Listener}
 
 import scala.collection.mutable
-import scala.collection.mutable.ArrayBuffer
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 import scala.jdk.FutureConverters._
@@ -30,16 +30,18 @@ class PlayerQuitListener extends Listener with CoordinationHookRegistry {
       .map(_.asScala)
       .toSeq
 
-    val redis: RedisPluginMessagesAPI = SpigotSemaphore.isntance().redisPluginMessagesAPI()
-    val playerName: String = event.getPlayer.getName
-    FuturesCompletionWaiting.waitAllFuturesCompletion(futures).onComplete {
-      case Success(_) =>
-        redis.sendRedisPluginMessage(SpigotSemaphore.PLUGIN_MESSAGING_CHANNEL, s"confirm_player_data_saved $playerName")
-      case Failure(ex) =>
-        println(s"${playerName}のプレイヤーデータの保存に失敗しました。")
-        ex.printStackTrace()
-        redis.sendRedisPluginMessage(SpigotSemaphore.PLUGIN_MESSAGING_CHANNEL, s"failed_saving_some_player_data $playerName")
-    }
+    Bukkit.getScheduler.runTask(SpigotSemaphore.isntance(), () => {
+      val redis: RedisPluginMessagesAPI = SpigotSemaphore.isntance().redisPluginMessagesAPI()
+      val playerName: String = event.getPlayer.getName
+      FuturesCompletionWaiting.waitAllFuturesCompletion(futures).onComplete {
+        case Success(_) =>
+          redis.sendRedisPluginMessage(SpigotSemaphore.PLUGIN_MESSAGING_CHANNEL, s"confirm_player_data_saved $playerName")
+        case Failure(ex) =>
+          println(s"${playerName}のプレイヤーデータの保存に失敗しました。")
+          ex.printStackTrace()
+          redis.sendRedisPluginMessage(SpigotSemaphore.PLUGIN_MESSAGING_CHANNEL, s"failed_saving_some_player_data $playerName")
+      }
+    })
   }
 
   override def register[U](save: QuitEventDataSaveHook[U]): Unit = hookList += save
